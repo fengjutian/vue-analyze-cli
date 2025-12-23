38d01a4d4aa03b2e8e125bf27ac8d02d4e068166
fengjutian <fjutian@foxmail.com>
2025-12-23 14:34:31 +0800

    Remove dist directory from Git tracking

--- dist/analyzer.d.ts
+++
@@ -1,4 +0,0 @@
-import type { TemplateInfo } from './type.d.ts';
-export declare function analyzeVueFile(filePath: string): TemplateInfo;
-export declare function analyzeProject(projectPath: string): TemplateInfo[];
-//# sourceMappingURL=analyzer.d.ts.map
--- dist/generateHtmlReport.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TemplateInfo } from './type.d.ts';
-export declare function generateHtmlReport(report: TemplateInfo[], outputPath: string): void;
-//# sourceMappingURL=generateHtmlReport.d.ts.map
